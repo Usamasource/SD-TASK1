a349f0ab8a383c507f0f4808647a45f9bd6d73bf
Ayman <aymanbourramouss@gmail.com>
2021-02-26 22:52:23 +0100

    test

--- master.py
+++ master.py
@@ -1,38 +1,32 @@
    
 import redis
-import multiprocessing
+from multiprocessing import Process
+from flask import Flask
 
-    WORKERS = {}
-    WORKER_ID = 0
+app = Flask(__name__)
 
-    def __init__(self,ip,port):
-        self.redis_connection=redis.Redis(
-            host=os.getenv("REDIS_HOST", ip),
-            port=os.getenv("REDIS_PORT", port),
-        )
-        self.redis_queue = Queue(connection=self.redis_connection)
-
-   
-
-    def start_worker(worker_id,url):
+WORKERS = {}
+WORKER_ID = 0
 
 
-    def create_worker():
-        global WORKERS
-        global WORKER_ID
-        proc = Process(target=start_worker, args=(WORKER_ID,url))
-        proc.start()
-        WORKERS[WORKER_ID]=proc
-        WORKER_ID=+1
+@app.route('/create')
+def create_worker():
+    global WORKERS
+    global WORKER_ID
+    proc = Process(target=start_worker, args=(url))
+    proc.start()
+    WORKERS[WORKER_ID]=proc
+    WORKER_ID=+1
 
-    def delete_worker():
-        global WORKERS
-        global WORKER_ID
-        WORKERS.remove(WORKER_ID)
-        WORKERS_ID=-1
+@app.route('/delete')
+def delete_worker():
+    global WORKERS
+    global WORKER_ID
+    WORKERS.remove(WORKER_ID)
+    WORKERS_ID=-1
+
+def start_worker():
     
-    def send_url(self, url_task):
-        job=self.redis_queue.enqueue("words_at_url", url_task)
-        print(job.result)
-        
-    
+
+if __name__ == '__main__':
+    app.run()
