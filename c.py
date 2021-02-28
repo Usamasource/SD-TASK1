83a5db21423654c2270c22967db26cb3aec1fe04
Usama <ubenabdelkrim2@gmail.com>
2021-02-28 03:18:21 +0100

    enlace cliente-server+workers corregidos

--- c.py
+++ c.py
@@ -6,11 +6,11 @@ import sys
 s = xmlrpc.client.ServerProxy('http://localhost:9000')
 arguments = sys.argv
 
-"""print(s.count(requests.get("http://localhost:8000/"+"ml/hola1.txt").text))
-print(s.wcount(requests.get("http://localhost:8000/"+"ml/hola1.txt").text))"""
+s.send_url("http://localhost:8000/hola.txt")
 
 
-if(len(arguments)<= 2):
+
+"""if(len(arguments)<= 2):
     print("!Error!: Has d'introduir més parametres. Format: client.py <workers> <create-delete> || client.py run ")
 else:
     if(arguments[1] == 'run'):
@@ -19,4 +19,4 @@ else:
         if(arguments[1] == 'create'):
             print("Creant...")
         if(arguments[1] == 'delete'):
-            print("Eliminant...")
+            print("Eliminant...")"""
--- s.py
+++ s.py
@@ -24,11 +24,13 @@ class Master():
     @app.route('/start_worker')
     def start_worker(self, funct1, funct2):
         while(1):
-            task=self.redis_connection.blpop('queue:tasks', 0)
+            task=self.redis_connection.blpop(['queue:tasks'], 0)
             if task:
-                data=requests.get(url)
-                n_words=funct1(data)
-                words_frequency=funct2(data)
+                data=requests.get(task[1]).text
+                n_words=counting_words(data)
+                words_frequency=word_count(data)
+                print(n_words)
+                print(words_frequency)
 
     @app.route('/create')
     def create_worker(self, funct1, funct2):
@@ -50,14 +52,15 @@ class Master():
         WORKERS_ID=-1
 
     def send_url(self, url_task):
-        job=self.redis_connection.rpush('queue:tasks', json.dumps(url_task))
+        job=self.redis_connection.rpush('queue:tasks', url_task)
 
 # Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/','/RPC2')
 
 def counting_words(text):
-        return len(text.split(" "))
+    print("------------------------------------"+text)
+    return len(text.split())
 
 def word_count(text):
     word_frequencies=[]
@@ -67,18 +70,27 @@ def word_count(text):
     return word_frequencies
 
 # Create server
-with SimpleXMLRPCServer(('localhost', 9000),
-    requestHandler=RequestHandler) as server:
+if __name__ == '__main__':
+    server=SimpleXMLRPCServer(('localhost', 9000),
+        requestHandler=RequestHandler,
+        logRequests=True,
+        allow_none=True)
+
     server.register_introspection_functions()
+    server.register_multicall_functions()
     master=Master("localhost","6379", server)
-    
-    for i in range(10):
+
+    for i in range(5):
         master.create_worker(counting_words, word_count)
 
-server.register_function(counting_words, 'count')
-server.register_function(word_count, 'wcount')
-# Run the server's main loop
-server.serve_forever()
+    server.register_instance(master)
+
+    # Run the server's main loop
+    try:
+        print('Use Control-C to exit')
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print('Exiting')
 
 
   
